dba0433682881d0558757241799a12b259679602
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2026-02-02 10:36:55 +0100

    Initial commit

---
+++ exercises/src/main/java/com/example/exercises/As0525U5Wd1Application.java
@@ -0,0 +1,13 @@
+package com.example.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class As0525U5Wd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(As0525U5Wd1Application.class, args);
+	}
+
+}
---
+++ exercises/src/test/java/com/example/exercises/As0525U5Wd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class As0525U5Wd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
